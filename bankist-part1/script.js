8255d22c729c490a89899c6df8c85ed71dc61477
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-05 17:10:47 +0700

    feat: challenge #4

--- bankist-part1/script.js
+++ bankist-part1/script.js
@@ -540,4 +540,121 @@ console.log(movements.includes(-130));
 
 const anyDeposit = movements.some((movement) => movement > 3000);
 console.log(anyDeposit);
+
+// Flat and Flatmap
+
+const arr = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+  ],
+  7,
+  8,
+];
+
+const accountsMovement = accounts
+  .flatMap((account) => account.movements)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(accountsMovement);
+*/
+
+/*
+
+// Challenge #4
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+
+const breeds = [
+  {
+    breed: "German Shepherd",
+    averageWeight: 32,
+    activities: ["fetch", "swimming"],
+  },
+  {
+    breed: "Dalmatian",
+    averageWeight: 24,
+    activities: ["running", "fetch", "agility"],
+  },
+  {
+    breed: "Labrador",
+    averageWeight: 28,
+    activities: ["swimming", "fetch"],
+  },
+  {
+    breed: "Beagle",
+    averageWeight: 12,
+    activities: ["digging", "fetch"],
+  },
+  {
+    breed: "Husky",
+    averageWeight: 26,
+    activities: ["running", "agility", "swimming"],
+  },
+  {
+    breed: "Bulldog",
+    averageWeight: 36,
+    activities: ["sleeping"],
+  },
+  {
+    breed: "Poodle",
+    averageWeight: 18,
+    activities: ["agility", "fetch"],
+  },
+];
+
+// 1. Store husky weight
+const huskyWeight = breeds.find((dog) => dog.breed === "Husky").averageWeight;
+
+// 2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+
+const dogBothActivities = breeds.find((dog) => {
+  if (
+    dog.activities.some((activitie) => activitie === "running") &&
+    dog.activities.some((activitie) => activitie === "fetch")
+  ) {
+    return dog;
+  }
+});
+
+// 3 & 4. Create an array "allActivities" of all the activities of all the dog breeds
+
+const allActivities = new Set(breeds.flatMap((dog) => dog.activities));
+
+// 5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+
+const swimmingAdjacent = new Set(
+  breeds
+    .filter((dog) =>
+      dog.activities.some((activitie) => activitie === "swimming")
+    )
+    .flatMap((dog) => dog.activities)
+);
+
+// 6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+
+const averageWeight = breeds
+  .flatMap((dog) => dog.averageWeight)
+  .every((weight) => weight >= 10);
+
+// 7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+const active = breeds.some((dog) => dog.activities.length >= 3);
+console.log(active);
+
+// BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator
+
+const heaviestBreed = Math.max(...breeds.flatMap((dog) => dog.averageWeight));
 */
