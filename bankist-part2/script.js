c0033999f3e7e7814bd507870177a6810a919073
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-08 15:05:55 +0700

    feat: implement modern date display

--- bankist-part2/script.js
+++ bankist-part2/script.js
@@ -21,9 +21,9 @@ const account1 = {
     "2020-01-28T09:15:04.904Z",
     "2020-04-01T10:17:24.185Z",
     "2020-05-08T14:11:59.604Z",
-    "2020-05-27T17:01:17.194Z",
-    "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2026-01-04T17:01:17.194Z",
+    "2026-01-05T10:36:17.929Z",
+    "2026-01-07T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -87,6 +87,23 @@ updateUi(currentAccount);
 containerApp.style.opacity = 1;
 
 // FUNCTION
+function formatMovementDate(date) {
+  const calcDayPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const dayPassed = calcDayPassed(new Date(), date);
+
+  if (dayPassed === 0) return `Today`;
+  if (dayPassed === 1) return `Yesterday`;
+  if (dayPassed <= 7) return `${dayPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
+
 // Display
 function displayMovements(currentAccount, sort = false) {
   const combineMovementAndDates = currentAccount.movements.map(
@@ -104,13 +121,8 @@ function displayMovements(currentAccount, sort = false) {
     .map((object, index) => {
       const { movement, movementDate } = object;
       const type = movement > 0 ? "deposit" : "withdrawal";
-
       const date = new Date(movementDate);
-      const day = `${date.getDate()}`.padStart(2, 0);
-      const month = `${date.getMonth() + 1}`.padStart(2, 0);
-      const year = date.getFullYear();
-
-      const displayDate = `${day}/${month}/${year}`;
+      const displayDate = formatMovementDate(date);
 
       return `
         <div class="movements__row">
@@ -448,3 +460,11 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 */
+
+const future = new Date(2037, 10, 19, 15, 23);
+// console.log(Number(future));
+
+const calcDayPassed = (date1, date2) => (date2 - date1) / (1000 * 60 * 60 * 24);
+const day1 = calcDayPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+
+// console.log(day1);
